142da343a8a26106dd42ed62652cf79bac6c5892
MadhavBorkar <157888465+MadhavBorkar@users.noreply.github.com>
2024-04-03 22:29:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function shortcut(s1, s2) {
   // your code here
+	if (str1.isEmpty() || str2.isEmpty()) {
+        return "";
+    }
+    char firstChar1 = str1.charAt(0);
+    char firstChar2 = str2.charAt(0);
+	
+    return String.valueOf(firstChar1) + String.valueOf(firstChar2);
 }
 
 // Do not change the code below.
